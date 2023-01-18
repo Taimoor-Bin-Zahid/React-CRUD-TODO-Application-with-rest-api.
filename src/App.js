b651ff4e182575f8a1cc35d4cd2c7d85fa918e72
TaimoorZahid86 <taimoorbinzahid86@gmail.com>
2023-01-18 15:55:09 +0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data,setData]=useState([])
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos").then((result)=>{
+      result.json().then((resp)=>{
+        //console.warn("result",resp)
+        setData(resp)
+      })
+    })
+  },[]);
+
+  function deleteUser(id){
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`,{
+      method: "DELETE"
+    }).then((result)=>{
+        result.json().then((resp)=>{
+          console.warn(resp)
+        })
+    })
+  }
+
+  console.warn(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='heading'>ToDo list</h1>
+      <table className='Table'>
+        <tr className='TableRow'>
+          <td id='todoHeadings' className='tableData'>User ID</td>
+          <td id='todoHeadings' className='tableData'>ID</td>
+          <td id='todoHeadings' className='tableData'>Title</td>
+          <td id='todoHeadings' className='tableData'>Completed</td>
+          <td id='todoHeadings' className='tableData'>Operations</td>
+        </tr>
+        {
+          data.slice(0,10).map((item)=>
+          <tr className='TableRow'>
+          <td className='tableData'>{item.userId}</td>
+          <td className='tableData'>{item.id}</td>
+          <td className='tableData'>{item.title}</td>
+          <td className='tableData'>{item.completed}</td>
+          <td className='tableData' onClick={()=>deleteUser(item.id)}><button>Delete</button></td>
+        </tr>
+          )
+        }
+      </table>
     </div>
   );
 }
