71521b0b162ed64fa49cb179b10e6cc275bca0f5
TaimoorZahid86 <taimoorbinzahid86@gmail.com>
2023-01-19 11:51:29 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,22 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import {
+  BrowserRouter,Route, Routes
+
+} from "react-router-dom";
+import Fetchapi from './Fetchapi';
+import Todos from './components/Todos';
 
 function App() {
 
-  const [data,setData]=useState([])
-
-  useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/todos").then((result)=>{
-      result.json().then((resp)=>{
-        //console.warn("result",resp)
-        setData(resp)
-      })
-    })
-  },[]);
-
-  function deleteUser(id){
-    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`,{
-      method: "DELETE"
-    }).then((result)=>{
-        result.json().then((resp)=>{
-          console.warn(resp)
-        })
-    })
-  }
-
-  console.warn(data);
   return (
-    <div className="App">
-      <h1 className='heading'>ToDo list</h1>
-      <table className='Table'>
-        <tr className='TableRow'>
-          <td id='todoHeadings' className='tableData'>User ID</td>
-          <td id='todoHeadings' className='tableData'>ID</td>
-          <td id='todoHeadings' className='tableData'>Title</td>
-          <td id='todoHeadings' className='tableData'>Completed</td>
-          <td id='todoHeadings' className='tableData'>Operations</td>
-        </tr>
-        {
-          data.slice(0,10).map((item)=>
-          <tr className='TableRow'>
-          <td className='tableData'>{item.userId}</td>
-          <td className='tableData'>{item.id}</td>
-          <td className='tableData'>{item.title}</td>
-          <td className='tableData'>{item.completed}</td>
-          <td className='tableData' onClick={()=>deleteUser(item.id)}><button>Delete</button></td>
-        </tr>
-          )
-        }
-      </table>
-    </div>
+    <div>
+      <Todos />
+    <BrowserRouter>
+  <Routes>
+    <Route path ="/Fetchapi" element={<Fetchapi/>} />
+  </Routes>
+  </BrowserRouter>
+  </div>
   );
 }
 
---
+++ src/Fetchapi.js
@@ -0,0 +1,57 @@
+import './Fetchapi.css';
+import { useEffect, useState } from 'react';
+
+
+import React from 'react'
+
+function Fetchapi() {
+    const [data,setData]=useState([])
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos").then((result)=>{
+      result.json().then((resp)=>{
+        //console.warn("result",resp)
+        setData(resp)
+      })
+    })
+  },[]);
+
+  function deleteUser(id){
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`,{
+      method: "DELETE"
+    }).then((result)=>{
+        result.json().then((resp)=>{
+          console.warn(resp)
+        })
+    })
+  }
+
+  console.warn(data);
+  return (
+    <div>
+        <h1 className='heading'>ToDo list</h1>
+      <table className='Table'>
+        <tr className='TableRow'>
+          <td id='todoHeadings' className='tableData'>User ID</td>
+          <td id='todoHeadings' className='tableData'>ID</td>
+          <td id='todoHeadings' className='tableData'>Title</td>
+          <td id='todoHeadings' className='tableData'>Completed</td>
+          <td id='todoHeadings' className='tableData'>Operations</td>
+        </tr>
+        {
+          data.slice(0,10).map((item)=>
+          <tr className='TableRow'>
+          <td className='tableData'>{item.userId}</td>
+          <td className='tableData'>{item.id}</td>
+          <td className='tableData'>{item.title}</td>
+          <td className='tableData'>{item.completed}</td>
+          <td className='tableData' onClick={()=>deleteUser(item.id)}><button>Delete</button></td>
+        </tr>
+          )
+        }
+      </table>
+    </div>
+  )
+}
+
+export default Fetchapi;
